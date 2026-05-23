5b742903f5f40758243b38aba839eb82a86ef2ef
Tiago <roldantiago18@gmail.com>
2026-05-23 19:48:13 -0300

    Primer commit: Estructura principal de Spring Boot

---
+++ src/test/java/com/tiago/gestion_turnos/GestionTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.gestion_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
